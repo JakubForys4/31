224228fa50caa69fb0dec01220e2a5a14ac50695
JakubForys4 <jakubforys09@gmail.com>
2025-04-02 13:58:54 +0000

    3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        int height = 5;  // You can change this value to adjust triangle size
+        int height = 5;  
         
         for (int i = 0; i < height; i++) {
-            // Print spaces
+            
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             
-            // Print stars
+            
             for (int k = height; k > i; k--) {
                 System.out.print("*");
             }
